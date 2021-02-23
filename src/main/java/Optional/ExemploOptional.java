8ae69916ecbf634ebf74e3572b61dde35ad522af
Lucas <lucaswillian2100@gmail.com>
2021-02-23 13:30:45 -0300

    Trabalhando com Optional

--- src/main/java/Optional/ExemploOptional.java
+++ src/main/java/Optional/ExemploOptional.java
@@ -1,4 +1,30 @@
 package Optional;
 
+import java.util.Optional;
+
 public class ExemploOptional {
+    public static void main(String[] args) {
+        Optional<String> optionalString = Optional.of("Valor opcional");
+        System.out.println("Valor está presente? "+optionalString.isPresent());
+
+        System.out.println("\nAtravés de ifPresent");
+        optionalString.ifPresent(System.out::println);
+
+        System.out.println("\nAtravés de lambda ifPresentOrElse");
+        optionalString.ifPresentOrElse(System.out::println,
+                () -> System.out.println("Valor não está presente"));
+
+        System.out.println("\nAtravés de if");
+        if (optionalString.isPresent()){
+            String valor = optionalString.get();
+            System.out.println(valor);
+        }
+
+        System.out.println("\nAtravés de map para poder modificar o valor, nesse caso acrescentar ****");
+        optionalString.map((valor)-> valor.concat("****")).ifPresent(System.out::println);
+
+
+        optionalString.orElseThrow(IllegalStateException::new);
+
+    }
 }
--- src/main/java/Optional/ExemploOptionalEstados.java
+++ src/main/java/Optional/ExemploOptionalEstados.java
@@ -1,9 +1,24 @@
 package Optional;
 
 import javax.swing.text.html.Option;
+import java.util.Optional;
 
 public class ExemploOptionalEstados {
     public static void main(String[] args) {
-        Option
+        Optional<String> optionalString = Optional.of("Valor presente");
+        System.out.println("Valor opcional que está presente:");
+        optionalString.ifPresentOrElse(System.out::println, () -> System.out.println("Não está presente"));
+
+        Optional<String> optionalNull = Optional.ofNullable(null);
+        System.out.println("\nValor opcional que não está presente:");
+        optionalNull.ifPresentOrElse(System.out::println, () -> System.out.println("null = não está presente"));
+
+        Optional<String> emptyOptional = Optional.empty();
+        System.out.println("\nValor opcional que não está presente:");
+        emptyOptional.ifPresentOrElse(System.out::println, () -> System.out.println("empty = não está presente"));
+
+        Optional<String> optionalNullErro = Optional.of(null);//ultiliza a ofNullable para nao dar erro
+        System.out.println("\nValor opcional que lança  erro NullPointerException");
+        optionalNullErro.ifPresentOrElse(System.out::println, () -> System.out.println("erro = não está presente"));
     }
 }
